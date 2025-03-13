951fc5684a459468de69df43f42af06af02877ed
lcarta/Luca Carta <lucarta@outlook.it>
2025-03-13 18:51:00 +0100

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+const checkButton = document.getElementById('check-btn');
+const explainText = document.getElementById('explain-text');
+const textInput = document.getElementById('text-input');
+const openIcon = document.getElementById('open-explain-icon');
+const resultBox = document.getElementById('result-box');
+const resultText = document.getElementById('result');
+let isOpenExplain = false;
 
+
+
+const isInvalidInput = (text) => {
+  const regex = /[/\w/i]/g;
+  return text.match(regex);
+}
+
+const palindromeChecker = () => {
+  const textInputArray = isInvalidInput(textInput.value);
+  const revTextArray = [...textInputArray].reverse();
+  textInputArray.join("") === revTextArray.join("") ? resultBox.style.backgroundColor = "var(--result-background-color-palindrome)" : resultBox.style.backgroundColor = "var(--result-background-color-not-palindrome)"
+  return textInputArray.join("") === revTextArray.join("") ? `"${textInput.value}" is a palindrome` : `"${textInput.value}" is not a palindrome`;
+}
+
+const viewReturn = () => {
+  if (textInput.value !== "") {
+    resultText.innerText = palindromeChecker();
+    textInput.value = "";
+    resultBox.style.display = "block";
+  } else {
+    window.alert('Please input a value');
+    resultBox.style.display = "none";
+    return
+  }
+}
+
+//Start Function for open explain text
+const openExplain = () => {
+  if (isOpenExplain) {
+    explainText.style.display = "none";
+    isOpenExplain = false;
+    openIcon.innerText = "keyboard_double_arrow_down";
+  }
+  else {
+    explainText.style.display = "block";
+    isOpenExplain = true;
+    openIcon.innerText = "keyboard_double_arrow_up";
+  }
+}
+
+
+
+//End Function for open explain text
+
+openIcon.addEventListener('click', openExplain);
+
+checkButton.addEventListener('click', viewReturn)
